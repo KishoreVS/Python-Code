9e15ec0df918eaf8653fb10aef5fd3d4885af17c
KishoreVS <virugauti100@gmail.com>
2017-11-06 21:24:08 +0530

    Python exercises

---
+++ hello.py
@@ -0,0 +1,9 @@
+choice=['ctf','ftc']
+num=input('Enter the temprature')
+def ctf(num):
+    return num*(9.0/5.0)+32
+def ftc(num):
+    return (num-32)*5.0/9.0
+if choice='ctf'
+print('Temp in farhenheit is:'+str(num))
+
---
+++ test.py
@@ -0,0 +1,11 @@
+
+def inputCheck(userChoice):
+    if userChoice == 1:
+        print('You Have Chosen to Enter Text to Encrypt!')
+        enterText()
+    if userChoice == 2:
+        print('You Have Chosen to Encypt Entered Text!')
+        encryptText()
+    if userChoice == 3:
+        print('You Have Chosen to Display Encypted Text!')
+        displayText()
